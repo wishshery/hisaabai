df9654827a636597379b849eebc0621942883694
wishshery <wishshery@yahoo.com>
2026-03-25 21:12:21 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE = 'hisaabai-v2';
+const ASSETS = [
+  '/hisaabai/',
+  '/hisaabai/index.html',
+  '/hisaabai/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.hostname === 'fonts.googleapis.com' || url.hostname === 'fonts.gstatic.com') {
+    e.respondWith(
+      fetch(e.request).then(r => {
+        const rc = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, rc));
+        return r;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(r => {
+        if (!r || r.status !== 200 || r.type !== 'basic') return r;
+        const rc = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, rc));
+        return r;
+      });
+    })
+  );
+});
